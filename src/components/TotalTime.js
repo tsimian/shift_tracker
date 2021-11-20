bc24223a35e18b0ac3882c1cd4102055213dffb0
Rob Lehmann <lehmannrobb@gmail.com>
2021-11-20 18:51:08 +0100

    made TotalTime dynamically load total hours and minutes

--- src/components/TotalTime.js
+++ src/components/TotalTime.js
@@ -1,8 +1,17 @@
 const TotalTime = ({ shifts }) => {
+    // Get all hours and minutes
+    const allHours = shifts.map(shift => parseInt(shift.hours))
+    const allMins = shifts.map(shift => parseInt(shift.minutes))
+
+    // Get sum of all hours and minutes
+    const totalHours = allHours.reduce((a, b) => a + b, 0)
+    const totalMins = allMins.reduce((a, b) => a + b, 0)
 
     return (
-        <div className="container d-flex justify-content-end">
+        <div className="container total-wrapper">
             <h5>Total Time</h5>
+            <p>{totalHours}</p>
+            <p>{totalMins}</p>
         </div>
     )
 }
