fc5be3ffbf2f171da308b5c937a3bcc3e26ddaf9
Rob Lehmann <lehmannrobb@gmail.com>
2021-11-21 20:39:24 +0100

    Fixed the bug in TotalTime and updated README

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Shift tracker
 - An app designed to keep track of hours at work
 
-## Bugs
-- Hours don't increment properly when minutes go above 60
-
 ## Todos
 - Create database
 - Create user login
+- Create separate months folders
--- src/components/TotalTime.js
+++ src/components/TotalTime.js
@@ -1,18 +1,22 @@
 const TotalTime = ({ shifts }) => {
+
     // Get all hours and minutes
     const allHours = shifts.map(shift => parseInt(shift.hours))
     const allMins = shifts.map(shift => parseInt(shift.minutes))
 
     // Get sum of all hours and minutes
-    let totalHours = allHours.reduce((a, b) => a + b, 0)
-    let totalMins = allMins.reduce((a, b) => a + b, 0)
+    let sumHours = allHours.reduce((a, b) => a + b, 0)
+    let sumMins = allMins.reduce((a, b) => a + b, 0)
+
+    // Convert sum of hours and minutes into minutes
+    const totalMins = (sumHours * 60) + sumMins
 
 
     return (
         <div className="container total-wrapper d-flex justify-content-evenly">
             <h5>Total Time</h5>
-            <p>{totalMins >= 60 ? totalHours += 1 : totalHours}</p>
-            <p>{totalMins >= 60 ? totalMins % 60 : totalMins}</p>
+            <p>{Math.floor(totalMins / 60)}</p>
+            <p>{totalMins % 60}</p>
         </div>
     )
 }
