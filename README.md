a6ab445990f6609c4118246f56b9cdee70aa9812
Rob Lehmann <lehmannrobb@gmail.com>
2021-11-23 14:53:22 +0100

    Added calender and set it as favicon.ico, added content to README.md

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 - Create database
 - Create user login
 - Create separate months folders
+
+## To Run
+```bash
+npm run server
+npm start
+```
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 const Footer = () => {
     return (
         <div className="text-center">
+            <hr/>
             <h6>&copy; 2021</h6>
         </div>
     )
